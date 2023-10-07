61753d5245765c71c431a60ffca26c353b45d4d6
stefan-jieanu <stefanalexjieanu@gmail.com>
2023-10-07 16:58:22 +0300

    final commit

--- main.js
+++ main.js
@@ -2,3 +2,14 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
+
+function abc() {
+  a = 5;
+  b = a + 2;
+
+  console.log(b);
+}
+
+abc()
+var a;
+var b;
